18cc67a2e23cdb9d72caf47762a27685dc733629
João Vítor <joaovitorsf7@hotmail.com>
2021-01-23 11:57:12 -0300

    web scraping.

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 def main():
     browser = webdriver.Firefox()
     browser.get("https://jjoaovitor7.github.io/")
-    dataHora = browser.find_element_by_class_name("data-hora")
+    dataHora = browser.find_element_by_class_name("data-hora").find_element_by_tag_name("p")
     print(dataHora.text)
 
 if __name__ == "__main__":
