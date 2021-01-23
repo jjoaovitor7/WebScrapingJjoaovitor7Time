13c58f36f1fa278f15149a15a2da4221dcc151b8
João Vítor <joaovitorsf7@hotmail.com>
2021-01-23 12:38:41 -0300

    getTime.py, api.py e .gitignore modificado.

---
+++ src/api.py
@@ -0,0 +1,12 @@
+from flask_api import FlaskAPI
+from getTime import getTime
+
+app = FlaskAPI(__name__)
+
+
+@app.route('/time/', methods=['GET'])
+def time():
+    return { 'time': getTime()}
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- src/getTime.py
+++ src/getTime.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
 
-def main():
+def getTime():
     browser = webdriver.Firefox()
     browser.get("https://jjoaovitor7.github.io/")
     dataHora = browser.find_element_by_class_name("data-hora").find_element_by_tag_name("p")
-    print(dataHora.text)
+    return dataHora.text
 
 if __name__ == "__main__":
-    main()
+    print(getTime())
