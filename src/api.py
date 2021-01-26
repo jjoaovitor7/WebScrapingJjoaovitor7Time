4cd42281388a1fc8d1b1fda3d60abd01a2fb8b8e
João Vítor <joaovitorsf7@hotmail.com>
2021-01-26 12:24:29 -0300

    swagger.

--- src/api.py
+++ src/api.py
@@ -1,11 +1,16 @@
 from flask_api import FlaskAPI
 from getTime import getTime
+from flasgger import Swagger
+from flasgger.utils import swag_from
 
 app = FlaskAPI(__name__)
 
 @app.route('/time/', methods=['GET'])
+@swag_from('./time.yml') # http://localhost:5000/apidocs/
 def time():
     return { 'time': getTime()}
+
+swag = Swagger(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
