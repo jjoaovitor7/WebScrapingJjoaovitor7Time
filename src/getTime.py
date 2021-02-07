a8098a4b55a527cc7e49ca2f00d6d79f656cba48
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2021-02-07 10:19:34 -0300

    fix.

--- src/getTime.py
+++ src/getTime.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 def getTime():
     browser = webdriver.Firefox()
     browser.get("https://jjoaovitor7.github.io/")
-    dataHora = browser.find_element_by_class_name("data-hora").find_element_by_tag_name("p")
+    dataHora = browser.find_element_by_class_name("dateHour").find_element_by_tag_name("p")
     return dataHora.text
 
 if __name__ == "__main__":
